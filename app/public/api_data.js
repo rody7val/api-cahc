24f9548036e0888f76bb3aa7b54a1b5baeaff4ee
rody7val <rod7val@gmail.com>
2017-01-09 20:25:40 -0300

    Códigos de estados HTTP agregados

--- app/public/api_data.js
+++ app/public/api_data.js
@@ -11,59 +11,24 @@ define({ "api": [
         "Success 200": [
           {
             "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "_id",
-            "description": "<p>Identificador de la noticia.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "title",
-            "description": "<p>Titulo de la noticia.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "content",
-            "description": "<p>Contenido pricipal.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "category",
-            "description": "<p>Categoria asociada.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "Boolean",
+            "type": "Number",
             "optional": false,
             "field": "status",
-            "description": "<p>Estado.</p>"
+            "description": "<p>Código de estado HTTP.</p>"
           },
           {
             "group": "Success 200",
-            "type": "String",
+            "type": "Object",
             "optional": false,
-            "field": "url_img",
-            "description": "<p>Dirección de la imagen.</p>"
-          },
-          {
-            "group": "Success 200",
-            "type": "Date",
-            "optional": false,
-            "field": "created",
-            "description": "<p>Fecha de creación.</p>"
+            "field": "notices",
+            "description": "<p>Arreglo de todas las noticia.</p>"
           }
         ]
       },
       "examples": [
         {
           "title": "Respuesta de ejemplo con dos noticias en la Base de Datos:",
-          "content": "[{\n    \"title\": \"Incentivando el tenis\",\n    \"content\": \"Próximamente la nueva cancha de tenis del club tendrá profesor. En breves ...\",\n    \"category\": \"DEPORTES\",\n    \"status\": false,\n    \"url_img\": \"https://i.imgur.com/WiaAYKn.gif\",\n    \"_id\": \"58730624f210341390a79fd4\",\n    \"created\": \"2017-01-19T02:31:13.000Z\"\n},\n    \"title\": \"Campeonato de Bochas\",\n    \"content\": \"El trío de bochas del Club Atlético Huracán de Chillar está participando del ...\",\n    \"category\": \"DEPORTES\",\n    \"status\": true,\n    \"url_img\": \"https://i.imgur.com/WiaAYKn.gif\",\n    \"_id\": \"58730624f210341390a79fd5\",\n    \"created\": \"2017-01-09T02:31:13.000Z\"\n}]",
+          "content": "{\n    \"status\": 200,\n    \"notices\": [{\n        \"title\": \"Incentivando el tenis\",\n        \"content\": \"Próximamente la nueva cancha de tenis del club tendrá profesor. En breves ...\",\n        \"category\": \"DEPORTES\",\n        \"status\": false,\n        \"url_img\": \"https://i.imgur.com/WiaAYKn.gif\",\n        \"_id\": \"58730624f210341390a79fd4\",\n        \"created\": \"2017-01-19T02:31:13.000Z\"\n    },\n        \"title\": \"Campeonato de Bochas\",\n        \"content\": \"El trío de bochas del Club Atlético Huracán de Chillar está participando del ...\",\n        \"category\": \"DEPORTES\",\n        \"status\": true,\n        \"url_img\": \"https://i.imgur.com/WiaAYKn.gif\",\n        \"_id\": \"58730624f210341390a79fd5\",\n        \"created\": \"2017-01-09T02:31:13.000Z\"\n    }]\n}",
           "type": "json"
         }
       ]
@@ -85,13 +50,6 @@ define({ "api": [
             "group": "Parameter",
             "type": "String",
             "optional": false,
-            "field": "_id",
-            "description": "<p>Identificador de la noticia.</p>"
-          },
-          {
-            "group": "Parameter",
-            "type": "String",
-            "optional": false,
             "field": "title",
             "description": "<p>Titulo de la noticia.</p>"
           },
@@ -114,7 +72,7 @@ define({ "api": [
             "type": "Boolean",
             "optional": false,
             "field": "status",
-            "description": "<p>Estado.</p>"
+            "description": "<p>Estado de moderación.</p>"
           },
           {
             "group": "Parameter",
@@ -122,22 +80,33 @@ define({ "api": [
             "optional": false,
             "field": "url_img",
             "description": "<p>Dirección de la imagen.</p>"
-          },
-          {
-            "group": "Parameter",
-            "type": "Date",
-            "optional": false,
-            "field": "created",
-            "description": "<p>Fecha de creación.</p>"
           }
         ]
       }
     },
     "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "status",
+            "description": "<p>Código de estado HTTP.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object",
+            "optional": false,
+            "field": "notice",
+            "description": "<p>Instancia de la noticia creada.</p>"
+          }
+        ]
+      },
       "examples": [
         {
           "title": "Respuesta de ejemplo al crear una noticia en la Base de Datos:",
-          "content": "{\n    \"title\": \"Incentivando el tenis\",\n    \"content\": \"Próximamente la nueva cancha de tenis del club tendrá profesor. En breves ...\",\n    \"category\": \"DEPORTES\",\n    \"status\": false,\n    \"url_img\": \"https://i.imgur.com/WiaAYKn.gif\",\n    \"_id\": \"58730624f210341390a79fd4\",\n    \"created\": \"2017-01-19T02:31:13.000Z\"\n}",
+          "content": "{\n    \"status\": 200,\n    \"notice\": {\n        \"title\": \"Incentivando el tenis\",\n        \"content\": \"Próximamente la nueva cancha de tenis del club tendrá profesor. En breves ...\",\n        \"category\": \"DEPORTES\",\n        \"status\": false,\n        \"url_img\": \"https://i.imgur.com/WiaAYKn.gif\",\n        \"_id\": \"58730624f210341390a79fd4\",\n        \"created\": \"2017-01-19T02:31:13.000Z\"\n    }\n}",
           "type": "json"
         }
       ]
